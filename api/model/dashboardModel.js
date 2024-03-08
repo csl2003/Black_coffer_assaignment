ce3bdd192844849da098ff62890a6d27d7fc38fa
csl2003 <140396011+csl2003@users.noreply.github.com>
2024-03-08 10:48:32 +0530

    Create dashboardModel.js

---
+++ api/model/dashboardModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const dashboardSchema = mongoose.Schema({
+  end_year: { type: Number },
+  intensity: { type: Number },
+  sector: { type: String },
+  topic: { type: String },
+  insight: { type: String },
+  url: { type: String },
+  region: { type: String },
+  start_year: { type: Number },
+  impact: { type: String },
+  published: { type: Date },
+  country: { type: String },
+  relevance: { type: Number },
+  pestle: { type: String },
+  source: { type: String },
+  title: { type: String },
+  likelihood: { type: Number },
+});
+
+const Dashboard = mongoose.model("Dashboard", dashboardSchema);
+
+module.exports = Dashboard;
